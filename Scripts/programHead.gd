86106d737ba8aae39f28af7d6adb9721ce328bb5
Kelyn (Desktop) <kelyn@crandall.me>
2019-05-28 13:23:26 -0500

    Added MoveGizmo instancing when selecting programs

--- Scripts/programHead.gd
+++ Scripts/programHead.gd
@@ -4,6 +4,7 @@ extends Area2D
 export(NodePath) var levelRef
 export(int) var tileX
 export(int) var tileY
+var moveGizmos = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,4 +22,12 @@ func _input_event(viewport, event, shape_idx):
 	and event.pressed:
 		print("Clicked")
 		var levelTiles = get_node(levelRef)
-		print(levelTiles.get_cell(tileX, tileY-2))
+		var gizScene = load("res://Databattle/MoveGizmo.tscn")
+		for i in [[tileX, tileY-1], [tileX, tileY+1], [tileX-1, tileY], [tileX+1, tileY]]:
+			print("Testing " + str(i))
+			if levelTiles.get_cell(i[0], i[1]) > 0:
+				print("Adding tile")
+				moveGizmos.append(gizScene.instance())
+				moveGizmos[-1].position.x = (i[0]+1)*32
+				moveGizmos[-1].position.y = -i[1]*32
+				get_node("/root").add_child(moveGizmos[-1])
