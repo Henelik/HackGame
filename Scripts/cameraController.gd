002512c112796e062f6de4c3ffb73709b730b1b1
Kelyn (Desktop) <kelyn@crandall.me>
2019-05-29 14:06:42 -0500

    Added player program tracking to camera

--- Scripts/cameraController.gd
+++ Scripts/cameraController.gd
@@ -6,11 +6,22 @@ var velocity = Vector2()
 export(Array, int) var playerTypes # 0 is local, 1 is AI, 2 is online
 var currentPlayer # an index of the array
 var selectedProgram
+var progs = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var programs = get_tree().get_nodes_in_group("Programs")
 	currentPlayer = 0
+	_populateProgArray()
+
+func _populateProgArray():
+	var progList = get_tree().get_nodes_in_group("Programs")
+	var tempProgs
+	for i in range(playerTypes.size()):
+		tempProgs = []
+		for p in progList:
+			if p.owningPlayerId == i:
+				tempProgs.append(p)
+		progs.append(tempProgs)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -20,4 +31,9 @@ func _physics_process(delta):
 
 func _on_EndTurnButton_pressed():
 	print("Ending turn...")
+	_nextTurn()
+	
+func _nextTurn():
+	currentPlayer = (currentPlayer+1)%playerTypes.size()
+	print("It is now player " + str(currentPlayer) + "'s turn.")
 	
