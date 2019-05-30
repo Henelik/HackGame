249010221aa4d070f9dc1f97efebf6e5f3360ffc
Kelyn (Desktop) <kelyn@crandall.me>
2019-05-30 12:06:15 -0500

    Added ability code

--- Scripts/Abilities/AttackAbility.gd
+++ Scripts/Abilities/AttackAbility.gd
@@ -1,4 +1,12 @@
 extends "res://Scripts/Abilities/BaseAbility.gd"
 
+export(int) var damage = 1
+
 func _ready():
+	pass
+
+func fireProgram(target):
+	target.damage(damage)
+	
+func fireTile(tile: Vector2):
 	pass
--- Scripts/Abilities/BaseAbility.gd
+++ Scripts/Abilities/BaseAbility.gd
@@ -1,15 +1,63 @@
 extends Node2D
 
-var apCost = 1 # number of ability points this consumes
-var abilityName = ""
+export(String) var abilityName = ""
+export(String, "enemy", "friend", "self", "level") var targetType # used for UI and AI
+export(int) var apCost = 1 # number of ability points this consumes
+export(int) var maxRange = 1
+export(Color) var gizmoColor
+var tileX
+var tileY
 var owningProgram
 var targetProgram
+var gizmos = []
 
 func _ready():
 	pass
 	
-func arm(): # arm the ability (e.g. when a player selects it)
+func checkRange(target: Vector2):
+	if abs(target.x-tileX)+abs(target.y-tileY) <= maxRange:
+		return true
+	return false
+
+func _select():
+	var gizScn = load("res://Databattle/Gizmo.tscn")
+	for x in range(tileX-maxRange, tileX+maxRange):
+		for y in range(tileY-maxRange, tileY+maxRange):
+			if abs(x-tileX)+abs(y-tileY) <= maxRange and not (x == tileX and y == tileY):
+				gizmos.append(gizScn.instance())
+				gizmos[-1].position.x = x*32
+				gizmos[-1].position.y = y*32
+				gizmos[-1].tileX = x
+				gizmos[-1].tileY = y
+				gizmos[-1].owningNode = self
+				gizmos[-1].setColor(gizmoColor)
+
+func _deselect():
+	for g in gizmos:
+		g.queue_free()
+	gizmos = []
+
+func findTarget(tile: Vector2):
+	var cam = get_node("../CamControl")
+	for player in cam.progs:
+		for p in player:
+			if Vector2(p.tileX, p.tileY) == tile:
+				fireProgram(p)
+				return
+			for t in p.tailSectors:
+				if Vector2(t.tileX, t.tileY) == tile:
+					fireProgram(p)
+					return
+	fireTile(tile) # no program occupies the targeted tile
+
+func fireProgram(target):
+	pass
+	
+func fireTile(tile: Vector2):
 	pass
 
-func fire(target):
-	pass
+func gizmoCallback(x, y):
+	if checkRange(Vector2(x, y)):
+		findTarget(Vector2(x, y))
+	else:
+		print("Target is out of range for " + abilityName)
