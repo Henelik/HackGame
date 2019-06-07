42043de3cf7cb00da66b5123947b2546093a66f3
Kelyn (Desktop) <kelyn@crandall.me>
2019-06-07 11:31:47 -0500

    Added Banhammer and Clog

---
+++ Scripts/Abilities/ClogAbility.gd
@@ -0,0 +1,13 @@
+extends "res://Scripts/Abilities/BaseAbility.gd"
+
+export(int) var slowAmount = 1
+
+func fireProgram(target):
+	if target.movePerTurn > 0:
+		target.movesPerTurn -= 1
+	get_node("AbilitySound").play()
+	_postFire()
+	
+func fireTile(tile: Vector2):
+	get_node("AbilitySound").play()
+	_postFire()
